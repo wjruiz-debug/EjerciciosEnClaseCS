9a0efe0c996b5c15d0e793b0d04573f7173b76da
wjruiz-debug <wjruiz@uamv.edu.ni>
2026-05-11 09:24:13 -0600

    feat resta de vectores

---
+++ magnitudvectoresecaso3/magnitudvectoresecaso3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+int [] vector1 = new int [5];
+double sumacuadrados = 0;
+double vectorcuadrado;
+double magnitudvectores;
+
+Console.WriteLine("Ingrese los elementos del vector: ");
+
+try
+{
+    for (int i = 0; i<vector1.Length;i++)
+    {
+        vector1[i]= int.Parse(Console.ReadLine()!);
+        vectorcuadrado =  Math.Pow(vector1[i],2);
+
+        sumacuadrados += vectorcuadrado;
+
+        Console.WriteLine($"Elemento {i +1 }: {vector1[i]} -> cuadrado: {vectorcuadrado} ");
+    }
+
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+
+magnitudvectores = Math.Sqrt(sumacuadrados);
+Console.WriteLine($"Magnitud del vector: {magnitudvectores}");
+
---
+++ restavectorescaso2/restavectorescaso2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+
+int [] vector1 = new int [5];
+int [] vector2 = new int [5];
+int [] diferenciavector= new int[5];
+
+Console.WriteLine("Ingrese los valores del primer vector: ");
+for (int i = 0; i < vector1.Length; i++)
+{
+    try
+    {
+        Console.Write($"Elemento {i+1}: ");
+        vector1[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada no valida, intente nuevamente.");
+        return;
+    }
+}
+    Console.WriteLine("Ingrese los valores del segundo vector: ");
+    for (int i = 0; i < vector2.Length; i++)
+    {
+        try
+        {
+            Console.Write($"Elemento {i+1}: ");
+            vector2[i] = int.Parse(Console.ReadLine()!);
+        }
+        catch(FormatException)
+        {
+            Console.WriteLine("Entrada no valida, intente nuevamente.");
+            return; 
+        }
+
+}
+
+    Console.WriteLine("La diferencia de los vectores, es de: ");
+    for (int i= 0; i < diferenciavector.Length; i++)
+    {
+        diferenciavector[i] = vector1[i] - vector2[i];
+    }
+{
+    for (int i = 0; i < diferenciavector.Length; i++)
+    {
+        Console.WriteLine($"Elemento {i+1}: {diferenciavector[i]}");
+    }
+}
